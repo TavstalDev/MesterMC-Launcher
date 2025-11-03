4a59d7f4c241bf1a82eaa2911d32d7d298079162
tavstal <tavstal@gmail.com>
2025-11-03 23:16:38 +0100

    Add LicenseWindow layout

---
+++ MMC-Installer/Views/LicenseWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Tavstal.MesterMC.Installer.Views;
+
+public partial class LicenseWindow : Window
+{
+    public LicenseWindow()
+    {
+        InitializeComponent();
+    }
+}
