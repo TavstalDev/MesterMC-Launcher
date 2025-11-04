e039c1ff285e4f12970857c1233aa7f954168aeb
tavstal <tavstal@gmail.com>
2025-11-04 15:46:24 +0100

    Update application theme to Light and include additional styles for inputs and checkboxes

--- MMC-Installer/Program.cs
+++ MMC-Installer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Avalonia;
+using ReactiveUI.Avalonia;
 
 namespace Tavstal.MesterMC.Installer;
 
@@ -17,5 +18,6 @@ class Program
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
-            .LogToTrace();
+            .LogToTrace()
+            .UseReactiveUI();
 }
