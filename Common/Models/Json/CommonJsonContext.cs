d2d57a9b1998414029767a6a5f3e4ce043377ba1
tavstal <tavstal@gmail.com>
2025-11-07 03:19:00 +0100

    Add CommonJsonContext for JSON serialization of CoreConfig and JavaMirrorConfig

---
+++ Common/Models/Json/CommonJsonContext.cs
@@ -0,0 +1,13 @@
+using System.Text.Json.Serialization;
+using Tavstal.KonkordLauncher.Common.Models.Config;
+using Tavstal.KonkordLauncher.Common.Models.Java;
+
+namespace Tavstal.KonkordLauncher.Common.Models.Json;
+
+[JsonSerializable(typeof(CoreConfig))]
+[JsonSerializable(typeof(JavaMirrorConfig))]
+[JsonSourceGenerationOptions(WriteIndented = true, IgnoreReadOnlyFields = true, IgnoreReadOnlyProperties = true)]
+public partial class CommonJsonContext : JsonSerializerContext
+{
+    
+}
