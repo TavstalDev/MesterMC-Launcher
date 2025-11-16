a9cc97a5d838033e36538cda89eb2b37041f9b61
tavstal <tavstal@gmail.com>
2025-11-16 17:54:48 +0100

    Add JsonSerializable attribute for List<NewsData> in CommonJsonContext

--- Common/Models/Json/CommonJsonContext.cs
+++ Common/Models/Json/CommonJsonContext.cs
@@ -6,6 +6,7 @@ namespace Tavstal.KonkordLauncher.Common.Models.Json;
 
 [JsonSerializable(typeof(CoreConfig))]
 [JsonSerializable(typeof(JavaMirrorConfig))]
+[JsonSerializable(typeof(List<NewsData>))]
 [JsonSourceGenerationOptions(WriteIndented = true, IgnoreReadOnlyFields = true, IgnoreReadOnlyProperties = true)]
 public partial class CommonJsonContext : JsonSerializerContext
 {
