09e2f11e3dea3eb3fd40211326cdeec44eecbbac
tavstal <tavstal@gmail.com>
2025-11-24 19:25:53 +0100

    Add AssemblyInfo and update project versioning for MMC-Installer

---
+++ MMC-Installer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MMC-Installer")]
+[assembly: AssemblyDescription("The official installer of MesterMC.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MesterMC")]
+[assembly: AssemblyProduct("MMC-Launcher")]
+[assembly: AssemblyCopyright("Copyright © 2025 MesterMC")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4C50CBD5-D4ED-4493-9421-9F8EA8BFAE9B")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
