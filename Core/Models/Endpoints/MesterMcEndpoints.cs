0a1ead4d7615b2f49356f8f3e4d64f95f003b907
tavstal <tavstal@gmail.com>
2025-11-26 17:38:04 +0100

    Update authentication endpoints for debug and production environments

--- Core/Models/Endpoints/MesterMcEndpoints.cs
+++ Core/Models/Endpoints/MesterMcEndpoints.cs
@@ -14,10 +14,18 @@ public static class MesterMcEndpoints
     /// The URL for retrieving all releases of Konkord.
     /// </summary>
     public const string AllReleases = "https://api.github.com/repos/TavstalDev/MMC-Launcher/releases";
+
+#if DEBUG
+    public const string AuthEndpoint = "http://localhost:36767/launcher/login";
     
+    public const string TfaEndpoint = "http://localhost:36767/launcher/tfa";
+    
+    public const string ApiBaseEndpoint = "http://localhost:36767/";
+#else
     public const string AuthEndpoint = "https://api.mestermc.hu/launcher/login";
     
-    public const string TfaEndpoint = "https://api.mestermc.hu/launcher/2fa";
+    public const string TfaEndpoint = "https://api.mestermc.hu/launcher/tfa";
     
     public const string ApiBaseEndpoint = "https://api.mestermc.hu/";
+#endif
 }
