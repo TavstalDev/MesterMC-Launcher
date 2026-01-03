5b96f5614741a1e8b34d1f32138016f362e4a0c7
Tavstal <tavstal@gmail.com>
2026-01-03 18:47:05 +0100

    Add AntiCheatService skeleton with verification notes and TODO for implementation

---
+++ Core/Services/AntiCheatService.cs
@@ -0,0 +1,23 @@
+namespace Tavstal.KonkordLauncher.Core.Services;
+
+public class AntiCheatService
+{
+    /* Notes:
+    # Files To Verify
+    - minecraft.jar
+    - version.jar
+    - mod related jars
+    
+    # Remove following arguments from JVM
+    - -Xdebug
+    - -javaagent
+    - -agentlib
+    - -Xrunjdwp
+    - -Xbootclasspath
+    - --add-opens
+       
+    */
+    
+    // TODO: Implement AntiCheatService
+    
+}
