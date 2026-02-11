8ff83fadb16c18f7094374761522a8822f2d49bf
tavstal <tavstal@gmail.com>
2026-02-11 21:29:47 +0100

    Refactor user controllers to inject dependencies for configuration, logging, user management, and database context

--- Api/Controllers/User/LauncherController.cs
+++ Api/Controllers/User/LauncherController.cs
@@ -1,9 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Tavstal.MesterMC.Api.Services.Database;
 
 namespace Tavstal.MesterMC.Api.Controllers.User;
 
 [Route("/launcher")]
 public class LauncherController : Controller
 {
+    private readonly IConfiguration _configuration;
+    private readonly ILogger _logger;
+    private readonly CustomUserManager _userManager;
+    private readonly CustomDbContext _dbContext;
     
+    public LauncherController(IConfiguration configuration, ILogger<LauncherController> logger, CustomUserManager userManager, CustomDbContext dbContext)
+    {
+        _configuration = configuration;
+        _logger = logger;
+        _userManager = userManager;
+        _dbContext = dbContext;
+    }
 }
--- Api/Controllers/User/ProfileController.cs
+++ Api/Controllers/User/ProfileController.cs
@@ -1,9 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Tavstal.MesterMC.Api.Services.Database;
 
 namespace Tavstal.MesterMC.Api.Controllers.User;
 
 [Route("/profile")]
 public class ProfileController : Controller
 {
+    private readonly IConfiguration _configuration;
+    private readonly ILogger _logger;
+    private readonly CustomUserManager _userManager;
+    private readonly CustomDbContext _dbContext;
     
+    public ProfileController(IConfiguration configuration, ILogger<ProfileController> logger, CustomUserManager userManager, CustomDbContext dbContext)
+    {
+        _configuration = configuration;
+        _logger = logger;
+        _userManager = userManager;
+        _dbContext = dbContext;
+    }
 }
