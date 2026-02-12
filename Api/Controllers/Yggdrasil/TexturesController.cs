cbf69acbfe799ecde5a35ea991a2784cf15f442c
Tavstal <tavstal@gmail.com>
2026-02-12 02:17:46 +0100

    Add TexturesController for retrieving texture data by hash

---
+++ Api/Controllers/Yggdrasil/TexturesController.cs
@@ -0,0 +1,37 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Tavstal.MesterMC.Api.Services.Database;
+using Tavstal.MesterMC.Api.Utils.Extensions;
+
+namespace Tavstal.MesterMC.Api.Controllers.Yggdrasil;
+
+[Route("/yggdrasil/textures")]
+public class TexturesController : Controller
+{
+    private readonly IConfiguration _configuration;
+    private readonly ILogger _logger;
+    private readonly CustomUserManager _userManager;
+    private readonly CustomDbContext _dbContext;
+
+    public TexturesController(IConfiguration configuration, ILogger<TexturesController> logger, CustomUserManager userManager, CustomDbContext dbContext)
+    {
+        _configuration = configuration;
+        _logger = logger;
+        _userManager = userManager;
+        _dbContext = dbContext;
+    }
+
+    [HttpGet("{hash}")]
+    public async Task<IActionResult> GetTexture(string hash)
+    {
+        var fileData = await _dbContext.FindFileDataAsync(x => x.Hash == hash);
+        if (fileData == null)
+          return this.ReturnResponseCode(HttpStatusCode.NotFound, "Texture not found.");
+        
+        byte[]? bytes = fileData.GetFileData();
+        if (bytes == null)
+          return this.ReturnResponseCode(HttpStatusCode.InternalServerError, "Failed to retrieve texture data.");
+        
+        return File(bytes, fileData.ContentType);
+    }
+}
