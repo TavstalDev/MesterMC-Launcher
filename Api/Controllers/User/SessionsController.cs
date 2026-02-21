44aa009ae7ff4cc848e228bdd9a78026854826e8
tavstal <tavstal@gmail.com>
2026-02-21 23:33:22 +0100

    Add session management endpoints to SessionsController for user and admin

--- Api/Controllers/User/SessionsController.cs
+++ Api/Controllers/User/SessionsController.cs
@@ -1,5 +1,8 @@
+using System.Net;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Tavstal.MesterMC.Api.Models.Database.User;
 using Tavstal.MesterMC.Api.Services.Database;
 
 namespace Tavstal.MesterMC.Api.Controllers.User;
@@ -11,17 +14,118 @@ public class SessionsController : CustomControllerBase
     private readonly CustomUserManager _userManager;
     private readonly CustomDbContext _dbContext;
     
-    // TODO: Add following endpoints:
-    // - GET /user/sessions: Get a list of active sessions for the current user.
-    // - DELETE /user/sessions/{sessionId}: Revoke a specific session by its ID (log out from that session).
-    // - DELETE /user/sessions: Revoke all sessions for the current user (log out from all devices).
-    // - GET /user/{id}/sessions: Get a list of active sessions for a specific user by their ID (admin only).
-    // - DELETE /user/{id}/sessions/{sessionId}: Revoke a specific session for a specific user by their ID (admin only).
-    // - DELETE /user/{id}/sessions: Revoke all sessions for a specific user by their ID (admin only).
-    
     public SessionsController(ILogger<SessionsController> logger, CustomUserManager userManager, CustomDbContext dbContext) : base(logger)
     {
         _userManager = userManager;
         _dbContext = dbContext;
     }
+
+    [HttpGet("sessions")]
+    public async Task<IActionResult> GetSessions()
+    {
+        CustomUser? user = await GetCurrentUserAsync(_userManager);
+        if (user == null)
+            return ReturnResponseCode(HttpStatusCode.Unauthorized, "User not authenticated");
+        
+        // TODO: Add claim check
+        
+        var userLogins = _dbContext.GetUserLogins(x => x.UserId == user.Id);
+        return ReturnJson(userLogins);
+    }
+
+    [HttpDelete("sessions/{sessionId}")]
+    public async Task<IActionResult> RevokeSession([BindRequired, FromQuery] ulong sessionId)
+    {
+        CustomUser? user = await GetCurrentUserAsync(_userManager);
+        if (user == null)
+            return ReturnResponseCode(HttpStatusCode.Unauthorized, "User not authenticated");
+        
+        // TODO: Add claim check
+        
+        var userLogin = _dbContext.FindUserLogin(x => x.Id == sessionId && x.UserId == user.Id);
+        if (userLogin == null)
+            return ReturnResponseCode(HttpStatusCode.NotFound, "Session not found.");
+        
+        await _dbContext.RemoveUserLoginAsync(userLogin, true);
+        return ReturnResponseCode(HttpStatusCode.OK, "Session revoked successfully.");
+    }
+
+    [HttpDelete("sessions")]
+    public async Task<IActionResult> RevokeAllSessions()
+    {
+        CustomUser? user = await GetCurrentUserAsync(_userManager);
+        if (user == null)
+            return ReturnResponseCode(HttpStatusCode.Unauthorized, "User not authenticated");
+        
+        // TODO: Add claim check
+        
+        await _dbContext.ClearUserLoginsAsync(user.Id, true);
+        return ReturnResponseCode(HttpStatusCode.OK, "All sessions revoked successfully.");
+    }
+    
+    #region Admin Endpoints
+    [HttpGet("{userId}/sessions")]
+    public async Task<IActionResult> GetSessionsAdmin([BindRequired, FromQuery] string userId)
+    {
+        CustomUser? user = await GetCurrentUserAsync(_userManager);
+        if (user == null)
+            return ReturnResponseCode(HttpStatusCode.Unauthorized, "User not authenticated");
+        
+        // TODO: Add claim check
+        
+        CustomUser? targetUser = await _userManager.FindByIdAsync(userId);
+        if (targetUser == null)
+            return ReturnResponseCode(HttpStatusCode.NotFound, "Target user not found");
+        
+        if (!_userManager.HasHigherRoleThan(user, targetUser))
+            return ReturnResponseCode(HttpStatusCode.Forbidden, "You do not have permission to manage this user.");
+        
+        var userLogins = _dbContext.GetUserLogins(x => x.UserId == targetUser.Id);
+        return ReturnJson(userLogins);
+    }
+
+    [HttpDelete("{userId}/sessions/{sessionId}")]
+    public async Task<IActionResult> RevokeSessionAdmin([BindRequired, FromQuery] string userId, [BindRequired, FromQuery] ulong sessionId)
+    {
+        CustomUser? user = await GetCurrentUserAsync(_userManager);
+        if (user == null)
+            return ReturnResponseCode(HttpStatusCode.Unauthorized, "User not authenticated");
+        
+        // TODO: Add claim check
+        
+        CustomUser? targetUser = await _userManager.FindByIdAsync(userId);
+        if (targetUser == null)
+            return ReturnResponseCode(HttpStatusCode.NotFound, "Target user not found");
+        
+        if (!_userManager.HasHigherRoleThan(user, targetUser))
+            return ReturnResponseCode(HttpStatusCode.Forbidden, "You do not have permission to manage this user.");
+        
+        var userLogin = _dbContext.FindUserLogin(x => x.Id == sessionId && x.UserId == targetUser.Id);
+        if (userLogin == null)
+            return ReturnResponseCode(HttpStatusCode.NotFound, "Session not found.");
+        
+        await _dbContext.RemoveUserLoginAsync(userLogin, true);
+        return ReturnResponseCode(HttpStatusCode.OK, "Session revoked successfully.");
+    }
+
+    [HttpDelete("{userId}/sessions")]
+    public async Task<IActionResult> RevokeAllSessionsAdmin([BindRequired, FromQuery] string userId)
+    {
+        CustomUser? user = await GetCurrentUserAsync(_userManager);
+        if (user == null)
+            return ReturnResponseCode(HttpStatusCode.Unauthorized, "User not authenticated");
+        
+        // TODO: Add claim check
+        
+        CustomUser? targetUser = await _userManager.FindByIdAsync(userId);
+        if (targetUser == null)
+            return ReturnResponseCode(HttpStatusCode.NotFound, "Target user not found");
+        
+        if (!_userManager.HasHigherRoleThan(user, targetUser))
+            return ReturnResponseCode(HttpStatusCode.Forbidden, "You do not have permission to manage this user.");
+        
+        await _dbContext.ClearUserLoginsAsync(targetUser.Id, true);
+        return ReturnResponseCode(HttpStatusCode.OK, "All sessions revoked successfully.");
+    }
+    #endregion
 }
