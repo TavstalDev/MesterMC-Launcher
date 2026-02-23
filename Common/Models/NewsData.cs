5d835e4ef07dbd46e0eeafb14c29f61555bca854
tavstal <tavstal@gmail.com>
2026-02-23 19:53:07 +0100

    Rename BannerPath to BannerUrl in NewsData model and update constructor

--- Common/Models/NewsData.cs
+++ Common/Models/NewsData.cs
@@ -1,6 +1,5 @@
 using System.Text.Json.Serialization;
 using Newtonsoft.Json;
-using Tavstal.KonkordLauncher.Core.Models.Endpoints;
 
 namespace Tavstal.KonkordLauncher.Common.Models;
 
@@ -11,17 +10,12 @@ public class NewsData
     [JsonPropertyName("content"), JsonProperty("content")]
     public string Content { get; set; }
     [JsonPropertyName("banner"), JsonProperty("banner")]
-    public string BannerPath { get; set; }
+    public string BannerUrl { get; set; }
     
-    public NewsData(string title, string content, string bannerPath)
+    public NewsData(string title, string content, string bannerUrl)
     {
         Title = title;
         Content = content;
-        BannerPath = bannerPath;
-    }
-    
-    public Uri  GetBannerUri()
-    {
-        return new Uri(new (MesterMcEndpoints.ApiBaseEndpoint), BannerPath);
+        BannerUrl = bannerUrl;
     }
 }
