5cca2059a77c17fe0632a556cc173c6f50964b49
tavstal <tavstal@gmail.com>
2026-02-25 22:31:16 +0100

    Add launcher version endpoints for retrieval and download

--- Api/Controllers/Launcher/LauncherController.cs
+++ Api/Controllers/Launcher/LauncherController.cs
@@ -1,27 +1,89 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Tavstal.MesterMC.Api.Models.Database.Launcher;
+using Tavstal.MesterMC.Api.Services;
 using Tavstal.MesterMC.Api.Services.Database;
 
 namespace Tavstal.MesterMC.Api.Controllers.Launcher;
 
+[ApiController]
 [Route("/launcher")]
 public class LauncherController : CustomControllerBase
 {
     private readonly CustomUserManager _userManager;
     private readonly CustomDbContext _dbContext;
+    private readonly MemoryCacheService _memoryCacheService;
+    private readonly TimeSpan CacheTTL = TimeSpan.FromHours(1);
     
     // TODO: Add following endpoints:
-    // - GET /launcher/versions: Get a list of available launcher versions.
-    // - GET /launcher/versions/latest: Get the latest launcher version.
     // - POST /launcher/version: Add a new launcher version (admin only).
     // - DELETE /launcher/version/{id}: Remove a launcher version (admin only).
-    // - GET /launcher/version/{id}: Get details of a specific launcher version.
     // - PATCH /launcher/version/{id}: Update details of a specific launcher version (admin only).
-    // - GET /launcher/version/{id}/download: Get a download link for a specific launcher version.
-    // - GET /launcher/version/{id}/changelog: Get the changelog for a specific launcher version.
     
-    public LauncherController(ILogger<LauncherController> logger, CustomUserManager userManager, CustomDbContext dbContext) : base(logger)
+    public LauncherController(ILogger<LauncherController> logger, CustomUserManager userManager, CustomDbContext dbContext, MemoryCacheService memoryCacheService) : base(logger)
     {
         _userManager = userManager;
         _dbContext = dbContext;
+        _memoryCacheService = memoryCacheService;
     }
+
+    [HttpGet("versions")]
+    public async Task<IActionResult> GetLauncherVersions()
+    {
+        var versions = await _dbContext.GetLauncherVersionsAsync();
+        if (!versions.Any())
+            return ReturnResponseCode(HttpStatusCode.NotFound, "No launcher versions found.");
+        
+        return ReturnJson(versions);
+    }
+
+    [HttpGet("versions/latest")]
+    public async Task<IActionResult> GetLatestLauncherVersion()
+    {
+        var version = await _dbContext.FindLatestLauncherVersionAsync();
+        if (version == null)
+            return ReturnResponseCode(HttpStatusCode.NotFound, "No launcher versions found.");
+        return ReturnJson(version);
+    }
+
+    [HttpGet("version/{id}")]
+    public async Task<IActionResult> GetLauncherVersionDetails([BindRequired, FromRoute] ulong id)
+    {
+        var version = await _dbContext.FindLauncherVersionAsync(x => x.Id == id);
+        if (version == null)
+            return ReturnResponseCode(HttpStatusCode.NotFound, "Launcher version not found.");
+        return ReturnJson(version);
+    }
+
+    [HttpGet("version/{id}/download")]
+    public async Task<IActionResult> GetLauncherVersionDownloadLink([BindRequired, FromRoute] ulong id, [BindRequired, FromQuery] ELauncherOs os)
+    {
+        string cacheKey = $"launcher_version:{id}:download_{os}";
+        if (_memoryCacheService.TryGetValue(cacheKey, out (byte[], string) cachedVersion))
+            return File(cachedVersion.Item1, cachedVersion.Item2);
+        
+        var version = await _dbContext.FindLauncherVersionAsync(x => x.Id == id);
+        if (version == null)
+            return ReturnResponseCode(HttpStatusCode.NotFound, "Launcher version not found.");
+        
+        var versionData = await _dbContext.FindLauncherVersionDataAsync(x => x.Os == os && x.VersionId == version.Id);
+        if (versionData == null)
+            return ReturnResponseCode(HttpStatusCode.NotFound, "Launcher version data not found.");
+
+        var fileData = await _dbContext.FindFileDataAsync(x => x.Id == versionData.FileId);
+        if (fileData == null || !fileData.Exists())
+            return  ReturnResponseCode(HttpStatusCode.NotFound, "File data not found.");
+
+        byte[]? bytes = fileData.GetFileData();
+        if (bytes == null)
+            return ReturnResponseCode(HttpStatusCode.InternalServerError, "Failed to retrieve the file.");
+        
+        _memoryCacheService.SetValue(cacheKey, (bytes, fileData.ContentType), CacheTTL);
+        return File(bytes, fileData.ContentType);
+    }
+
+    #region Admin Endpoints
+    
+    #endregion
 }
