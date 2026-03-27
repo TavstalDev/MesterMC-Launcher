850e2f4f4b0ba4df2bde3d2ff4363ef0485041a8
tavstal <tavstal@gmail.com>
2026-03-27 22:18:05 +0100

    Remove unused claim types related to login, email recovery, and two-factor authentication

--- Api/Models/Claims/CustomClaimTypes.cs
+++ Api/Models/Claims/CustomClaimTypes.cs
@@ -9,93 +9,9 @@ public static class CustomClaimTypes
     /// Claim type for user badges.
     /// </summary>
     public const string Badge = "mmc.User.Badge";
-
-    #region Login
-
-    /// <summary>
-    /// Claim type for tracking the number of login attempts.
-    /// </summary>
-    public const string LoginAttempt = "mmc.Login.Attempt";
-
-    /// <summary>
-    /// Claim type for the expiration time of login attempts.
-    /// </summary>
-    public const string LoginAttemptExpiration = "mmc.Login.AttemptExpiration";
-    #endregion
-        
-    #region Email
+    
     /// <summary>
     /// Claim type for the email confirmation token.
     /// </summary>
     public const string EmailConfirmationToken = "mmc.Email.ConfirmationToken";
-        
-    /// <summary>
-    /// Claim type for the email recovery expiration.
-    /// </summary>
-    public const string EmailRecoveryExpiration = "mmc.Email.RecoveryExpiration";
-        
-    /// <summary>
-    /// Claim type for the email recovery token.
-    /// </summary>
-    public const string EmailRecoveryToken = "mmc.Email.RecoveryToken";
-        
-    /// <summary>
-    /// Claim type for tracking the number of email recovery attempts.
-    /// </summary>
-    public const string EmailRecoveryAttempt = "mmc.Email.RecoveryAttempt";
-
-    /// <summary>
-    /// Claim type for the expiration time of email recovery attempts.
-    /// </summary>
-    public const string EmailRecoveryAttemptExpiration = "mmc.Email.RecoveryAttemptExpiration";
-    #endregion
-        
-    #region 2FA
-
-    /// <summary>
-    /// Claim type for the two-factor authentication recovery code.
-    /// </summary>
-    public const string TwoFactorRecoveryCode = "mmc.TwoFactor.RecoveryCode";
-        
-    /// <summary>
-    /// Claim type for tracking the number of recovery attempts for two-factor authentication.
-    /// </summary>
-    public const string TwoFactorRecoveryAttemptCount = "mmc.TwoFactor.RecoveryAttempt";
-
-    /// <summary>
-    /// Claim type for the expiration time of recovery attempts for two-factor authentication.
-    /// </summary>
-    public const string TwoFactorRecoveryAttemptExpiry = "mmc.TwoFactor.RecoveryAttemptExpiration";
-        
-    /// <summary>
-    /// Claim type for the two-factor authentication session token.
-    /// </summary>
-    public const string TwoFactorSessionToken = "mmc.TwoFactor.SessionToken";
-        
-    /// <summary>
-    /// Claim type for the two-factor authentication session expiration.
-    /// </summary>
-    public const string TwoFactorSessionExpiration = "mmc.TwoFactor.SessionExpiration";
-        
-    /// <summary>
-    /// Claim type for tracking the number of session attempts for two-factor authentication.
-    /// </summary>
-    public const string TwoFactorSessionAttempt = "mmc.TwoFactor.SessionAttempt";
-    
-    /// <summary>
-    /// Claim type for the two-factor authentication launcher session token.
-    /// </summary>
-    public const string TwoFactorLauncherSessionToken = "mmc.TwoFactor.Launcher.SessionToken";
-
-    /// <summary>
-    /// Claim type for the expiration time of the two-factor authentication launcher session.
-    /// </summary>
-    public const string TwoFactorLauncherSessionExpiration = "mmc.TwoFactor.Launcher.SessionExpiration";
-
-    /// <summary>
-    /// Claim type for tracking the number of session attempts for the two-factor authentication launcher.
-    /// </summary>
-    public const string TwoFactorLauncherSessionAttempt = "mmc.TwoFactor.Launcher.SessionAttempt";
-
-    #endregion
 }
