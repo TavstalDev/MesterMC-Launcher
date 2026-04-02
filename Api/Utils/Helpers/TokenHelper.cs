fb0698671f2331711951e2fb1645ca6066b43398
tavstal <tavstal@gmail.com>
2026-04-02 23:00:28 +0200

    Refactor TokenHelper to remove JWT generation methods; rename recovery token methods for clarity

--- Api/Utils/Helpers/TokenHelper.cs
+++ Api/Utils/Helpers/TokenHelper.cs
@@ -1,8 +1,4 @@
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 using System.Security.Cryptography;
-using System.Text;
-using Microsoft.IdentityModel.Tokens;
 using Tavstal.MesterMC.Api.Utils.Extensions;
 
 namespace Tavstal.MesterMC.Api.Utils.Helpers;
@@ -28,26 +24,6 @@ public static class TokenHelper
     }
 
     /// <summary>
-    /// Generates a JSON Web Token (JWT) using the specified parameters.
-    /// </summary>
-    /// <param name="encryptionKey">The encryption key used to sign the token.</param>
-    /// <param name="issuer">The issuer of the token.</param>
-    /// <param name="audience">The audience for which the token is intended.</param>
-    /// <param name="expireDate">The expiration date of the token.</param>
-    /// <param name="claims">Optional claims to include in the token.</param>
-    /// <returns>A string representation of the generated JWT.</returns>
-    public static string GenerateJwtToken(string encryptionKey, string issuer, string audience, DateTimeOffset expireDate, IEnumerable<Claim>? claims = null)
-    {
-        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(encryptionKey));
-        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-        var token = new JwtSecurityToken(
-            new JwtHeader(credentials),
-            new JwtPayload(issuer, audience, claims, DateTime.UtcNow, expireDate.UtcDateTime)
-        );
-        return new JwtSecurityTokenHandler().WriteToken(token);
-    }
-
-    /// <summary>
     /// Generates a random token with the specified byte length.
     /// </summary>
     /// <param name="tokenByteLength">The length of the token in bytes.</param>
@@ -68,16 +44,10 @@ public static class TokenHelper
     public static string GenerateAccountConfirmationToken() => GenerateToken();
 
     /// <summary>
-    /// Generates a recovery token.
+    /// Generates a recovery session token.
     /// </summary>
     /// <returns>A recovery token as a base64-encoded string.</returns>
-    public static string GenerateRecoveryToken() => GenerateToken();
-
-    /// <summary>
-    /// Generates a two-factor authentication token.
-    /// </summary>
-    /// <returns>A two-factor authentication token as a base64-encoded string.</returns>
-    public static string GenerateTwoFactorToken() => GenerateToken(20);
+    public static string GenerateRecoverySessionToken() => GenerateToken();
 
     /// <summary>
     /// Generates a two-factor session token.
@@ -86,17 +56,11 @@ public static class TokenHelper
     public static string GenerateTwoFactorSessionToken() => GenerateToken(32);
     
     /// <summary>
-    /// Generates a recovery code.
-    /// </summary>
-    /// <returns>A recovery code as a base64-encoded string.</returns>
-    public static string GenerateRecoveryCode() => GenerateToken(8);
-    
-    /// <summary>
     /// Generates a list of recovery codes.
     /// </summary>
     /// <param name="count">The number of recovery codes to generate.</param>
     /// <returns>A list of recovery codes.</returns>
-    public static List<string> GenerateRecoveryCodes(int count = 8)
+    public static List<string> GenerateRecoveryCodes(int count = 6)
     {
         List<string> recoveryCodes = [];
         for (int i = 0; i < count; i++)
