33a091ad4796e67b399a4917ceb3c4696d82dde3
Tavstal <tavstal@gmail.com>
2026-04-04 00:26:58 +0200

    Add clock skew and lockout settings to Settings constructor

--- Api/Models/Settings.cs
+++ Api/Models/Settings.cs
@@ -152,6 +152,9 @@ public class Settings
     /// <param name="encryptionKey">The symmetric key used for JWT signing/encryption.</param>
     /// <param name="issuer">The JWT issuer value.</param>
     /// <param name="audience">The JWT audience value.</param>
+    /// <param name="clockSkew">The clock skew tolerance for JWT token validation.</param>
+    /// <param name="lockoutMaxAttempts">The amount of failed authentication attempts allowed before a user is locked out.</param>
+    /// <param name="lockoutDuration">The duration of the lockout applied when the user exceeds the allowed failed authentication attempts.</param>
     /// <param name="emailProvider">The SMTP server hostname or provider identifier used for sending email.</param>
     /// <param name="emailPort">The SMTP port used to send email (commonly 25, 465, or 587).</param>
     /// <param name="emailAddress">The email address used as the sender for outgoing messages.</param>
@@ -162,7 +165,7 @@ public class Settings
     /// <param name="serverName">The server name presented by Yggdrasil-compatible endpoints.</param>
     /// <param name="implementationName">The name of the Yggdrasil implementation (metadata shown to clients).</param>
     /// <param name="implementationVersion">The version string of the Yggdrasil implementation.</param>
-    public Settings(string websiteUrl, string apiUrl, string databaseUser, string databasePassword, string encryptionKey, string issuer, string audience, string emailProvider, int emailPort, string emailAddress, string emailPassword, string[] skinDomains, string certPassword, byte[] cert, string serverName, string implementationName, string implementationVersion)
+    public Settings(string websiteUrl, string apiUrl, string databaseUser, string databasePassword, string encryptionKey, string issuer, string audience, TimeSpan clockSkew, int lockoutMaxAttempts, TimeSpan lockoutDuration, string emailProvider, int emailPort, string emailAddress, string emailPassword, string[] skinDomains, string certPassword, byte[] cert, string serverName, string implementationName, string implementationVersion)
     {
         WebsiteUrl = websiteUrl;
         ApiUrl = apiUrl;
@@ -171,6 +174,9 @@ public class Settings
         EncryptionKey = encryptionKey;
         Issuer = issuer;
         Audience = audience;
+        ClockSkew = clockSkew;
+        LockoutMaxAttempts = lockoutMaxAttempts;
+        LockoutDuration = lockoutDuration;
         EmailProvider = emailProvider;
         EmailPort = emailPort;
         EmailAddress = emailAddress;
