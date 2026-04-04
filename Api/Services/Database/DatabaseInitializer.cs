c6d301b5f870527234d75806d6db09689ae68c30
tavstal <tavstal@gmail.com>
2026-04-04 18:19:14 +0200

    Add DatabaseInitializer to set up custom database context and default roles

---
+++ Api/Services/Database/DatabaseInitializer.cs
@@ -0,0 +1,51 @@
+﻿using Tavstal.MesterMC.Api.Models.Claims;
+using Tavstal.MesterMC.Api.Models.Database.User;
+using Tavstal.MesterMC.Api.Models.Database.User.Claims;
+
+namespace Tavstal.MesterMC.Api.Services.Database;
+
+/// <summary>
+/// Provides functionality to initialize the custom database context.
+/// </summary>
+public static class DatabaseInitializer
+{
+    /// <summary>
+    /// Initializes the database by ensuring it is created and populating default data.
+    /// </summary>
+    /// <param name="context">The custom database context to initialize.</param>
+    /// <param name="userStore">The custom user store used for managing user data.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    public static async Task InitializeAsync(CustomDbContext context, CustomUserStore userStore, CancellationToken cancellationToken = default)
+    {
+        // Ensures the database is created.
+        await context.Database.EnsureCreatedAsync(cancellationToken);
+        
+        // Checks if roles are empty and adds default roles.
+        var roles = await userStore.Roles.QueryAsync(null, cancellationToken);
+        if (!roles.Any())
+        {
+            await userStore.Roles.AddRangeAsync([
+                new(1, "Default", "DEFAULT"),
+                new (90, "Moderator", "MODERATOR"),
+                new(100, "Admin", "ADMIN"),
+            ], true, cancellationToken);
+
+            Dictionary<string, CustomRole> roleCache = new();
+                 
+            // Adds claims to roles based on predefined role claims.
+            foreach (var roleClaims in CustomRoleClaims.Claims)
+            {
+                if (!roleCache.TryGetValue(roleClaims.Key, out CustomRole? role))
+                {
+                    role = await userStore.Roles.FindAsync(x => x.NormalizedName == roleClaims.Key, cancellationToken);
+                    if (role == null)
+                        continue;
+                    roleCache[roleClaims.Key] = role;
+                }
+
+                await userStore.RoleClaims.AddRangeAsync(RoleClaim.ToList(roleClaims.Value.ToList(), role.Id), cancellationToken: cancellationToken);
+            }
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
